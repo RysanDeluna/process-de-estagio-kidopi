011fbe3789f64ae7e9c84c4f6ac3079636ba4a63
RysanDeluna <ry.marco.andrade@gmail.com>
2024-04-12 21:23:31 -0300

    Funcoes para conversar com a API e Mostrar os dados encontrados

--- js/index.js
+++ js/index.js
@@ -1,21 +1,48 @@
+// Definição de variáveis auxiliares
 const botaoBR = document.getElementById("buttonBrazil");
 const botaoCa = document.getElementById("buttonCanada");
 const botaoAu = document.getElementById("buttonAustralia");
-
 const url_BR = "https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Brazil";
 const url_Ca = "https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Canada";
 const url_Au = "https://dev.kidopilabs.com.br/exercicio/covid.php?pais=Australia";
 
+// Criação dos Listeners
 botaoBR.addEventListener("click", () => {getData(url_BR)});
 botaoCa.addEventListener("click", () => {getData(url_Ca)});
 botaoAu.addEventListener("click", () => {getData(url_Au)});
 
-function getData(url) {
+// Aquisição de dados da API
+async function getData(url) {
     console.log(url)
-    const resposta = fetch(url, {method: 'GET'}).then(resposta => resposta.json());
-    resposta.then(dados => {console.log(dados)});
-    alert("Resposta adquirida!");
 
-    return resposta;
+    const response = await fetch(url)
+    const data = await response.json();
+
+    showData(data);
 }
+
+function populateTable() {
+
+}
+
+// Função auxiliar para ver os dados
+function showData(obj) {
+    for(const i in obj) {
+        console.log([obj[i].ProvinciaEstado, obj[i].Confirmados, obj[i].Mortos ]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
